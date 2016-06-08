fbaee31fa48d91d35b9db78688610b7aa3ac1b78
Migelo <Miha@Mihas-MBP.lan>
2016-06-08 22:23:40 +0200

    Posodobljen class Točka
    End točka drži trojico podatkov, ker smo v 3 dimenzijah.

--- src/Tocka.java
+++ src/Tocka.java
@@ -1,4 +1,12 @@
-
+@SuppressWarnings("unused")
 public class Tocka {
+	
+	private double x,y,z;
 
+	public Tocka(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
 }
