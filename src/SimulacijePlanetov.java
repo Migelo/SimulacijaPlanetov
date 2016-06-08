a67ee78fa3fff78fd413dfb323537584ffda2dff
Migelo <Miha@Mihas-MBP.lan>
2016-06-08 22:33:20 +0200

    Testiranje inicializacije
    Testni podatki za testiranje debuggerja.

--- src/SimulacijePlanetov.java
+++ src/SimulacijePlanetov.java
@@ -4,6 +4,8 @@ public class SimulacijePlanetov {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//nastavi začetne položaje in hitrosti
+		Planet a = new Planet(null, null, null);
+		a.polozaj(1,1,1);
+		System.out.println("Test");
 	}
-
 }
