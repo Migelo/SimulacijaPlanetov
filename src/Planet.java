3d2d2784798831a0f0e81a1231d45e0c9f04b128
Migelo <Miha@Mihas-MBP.lan>
2016-06-08 22:36:21 +0200

    Izboljšava class-a Planet
    Šumniki zamenjani z računalnikom prijaznimi črkami. Dodane funkcije za
    Planet.

--- src/Planet.java
+++ src/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
 	//koordinate, hitrosti in pospeški
 	private Tocka polozaj;
 	private Tocka hitrost;
-	private Tocka pospešek;
+	private Tocka pospesek;
 
 	//zgodovina premikov
 	private Vector<Tocka> zgodovina;
@@ -13,15 +13,37 @@ public class Planet {
 	public Planet(Tocka polozaj, Tocka hitrost, Tocka pospesek){
 		this.polozaj = polozaj;
 		this.hitrost = hitrost;
-		this.pospešek = pospesek;
+		this.pospesek = pospesek;
+	}
+
+
+	public void polozaj(double x, double y, double z){
+		this.polozaj(x,y,z);
+	}
+
+	public Tocka vrniPolozaj(){
+		return this.polozaj;
+	}
+	public Tocka vrniHitrost(){
+		return this.hitrost;
+	}
+	public Tocka vrniPospesek(){
+		return this.pospesek;
+	}
+
+	public void premakni(double dx, double dy, double dz){
+		this.polozaj = this.polozaj.pristej(dx, dy, dz);
 	}
 	
-	
-public void polozaj(double x, double y, double z){
-	this.polozaj(1,1,1);
-}
-
-//public void premakni(double dx, double dy, double dz){
-//	this.polozaj.
-//}
+	public void nastavi(int izbira, Tocka podatki){
+		if (izbira == 1){
+			this.polozaj = podatki;
+		}
+		if (izbira == 2){
+			this.hitrost = podatki;
+		}
+		if (izbira == 3){
+			this.pospesek = podatki;
+		}
+	}
 }
