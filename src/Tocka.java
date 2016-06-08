f88becf475d1baf55622b4700346223bedbbd53b
Migelo <Miha@Mihas-MBP.lan>
2016-06-08 22:34:41 +0200

    Izboljšava class-a Točka
    Dodane funkcije, ki se bodo uporabljale v drugih delih programa.

--- src/Tocka.java
+++ src/Tocka.java
@@ -1,4 +1,3 @@
-@SuppressWarnings("unused")
 public class Tocka {
 	
 	private double x,y,z;
@@ -7,6 +6,30 @@ public class Tocka {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+	}
+	public Tocka pristej(double x, double y, double z) {
+		this.x += x;
+		this.y += y;
+		this.z += z;
+		return null;
+	}
+	@Override
+	public String toString() {
+		return "Tocka [x=" + x + ", y=" + y + ", z=" + z + "]";
+	}
+	public double vrniElement(int i){
+		if (i == 1){
+			return this.x;
+		}
+		else if (i == 2){
+			return this.y;
+		}
+		else if (i == 3){
+			return this.z;
+		}
+		else{
+			throw new IllegalArgumentException("n mora biti med 1 in 3.");
+		}
 	}
 	
 }
