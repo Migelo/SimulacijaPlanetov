9b0a63544c761fdc356ca59450ece8e92adab565
Migelo <Miha@Mihas-MBP.lan>
2016-06-08 22:39:49 +0200

    Dodatno testiranje
    Testiranje inicializacije programa in samega delovanja.

--- src/SimulacijePlanetov.java
+++ src/SimulacijePlanetov.java
@@ -1,11 +1,18 @@
-
 public class SimulacijePlanetov {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//nastavi začetne položaje in hitrosti
-		Planet a = new Planet(null, null, null);
-		a.polozaj(1,1,1);
-		System.out.println("Test");
+		//Planet a = new Planet(Tocka(1,2,3), null, null);
+		//a.polozaj(1,1,1);
+		Tocka a = new Tocka(0, 0, 0);
+		Tocka c = new Tocka(5,5,5);
+		a.pristej(1,2,3);
+		Planet b = new Planet(a,a,a);
+		b.nastavi(1, c);
+		Racunanje.sila(b.vrniPolozaj());
+		System.out.println(Racunanje.sila(b.vrniPolozaj()));
+		System.out.println(a);
+		
 	}
 }
