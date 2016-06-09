e44e9bdd0a7d7a4589cc0ff8a033528f41e0defc
Migelo <Miha@Mihas-MBP.lan>
2016-06-10 00:00:31 +0200

    Implementacija nove funkcije Sila
    Nova funkcija sila vzame objekt Planet in izračuna njegov nov položaj,
    hitrost in pospešek.

--- src/Racunanje.java
+++ src/Racunanje.java
@@ -1,13 +1,5 @@
-import java.util.ArrayList;
-
 public class Racunanje {
-	public static double naslednjiKorak(double x, double y, double z) {
-		ArrayList<Double> koordinate = new ArrayList<Double>();
-		for (Double i : koordinate ){
-			
-		}
-		return x;
-	}
+	@SuppressWarnings("unused")
 	public static double sila(Tocka polozaj) {
 		double sila, silaX, silaY, silaZ = 0;
 		double radij = Math.sqrt(Math.pow(polozaj.vrniElement(1), 2) + Math.pow(polozaj.vrniElement(2), 2) + Math.pow(polozaj.vrniElement(3), 2));
@@ -17,8 +9,29 @@ public class Racunanje {
 		silaZ = polozaj.vrniElement(3)/radij;
 		return sila;
 	}
+	public static Planet sila2(Planet planet) {
+		double dt = 0.001;
+		double sila, silaX, silaY, silaZ = 0;
+		Tocka polozaj = planet.vrniPolozaj();
+		//double radij = Math.sqrt(Math.pow(polozaj.vrniElement(1), 2) + Math.pow(polozaj.vrniElement(2), 2) + Math.pow(polozaj.vrniElement(3), 2));
+		double radij = radij(polozaj);
+		sila = -1/Math.pow(radij,3);		
+		silaX = sila * polozaj.vrniElement(1) / radij;
+		silaY = sila * polozaj.vrniElement(2) / radij;
+		silaZ = sila * polozaj.vrniElement(3) / radij;
+		Tocka silaTocka = new Tocka(silaX, silaY, silaZ);
+		Tocka novaHitrost = new Tocka(silaX * dt + planet.vrniHitrost().vrniElement(1), silaY * dt + planet.vrniHitrost().vrniElement(2), silaZ * dt + planet.vrniHitrost().vrniElement(3));
+		Tocka novPremik = new Tocka(novaHitrost.vrniElement(1) * dt + planet.vrniPolozaj().vrniElement(1), novaHitrost.vrniElement(2) * dt + planet.vrniPolozaj().vrniElement(2), novaHitrost.vrniElement(3) * dt + planet.vrniPolozaj().vrniElement(3));
+		planet.nastavi(1, novPremik);
+		planet.nastavi(2, novaHitrost);
+		planet.nastavi(3, silaTocka);
+		planet.getZgodovina().add(novPremik);
+		return planet;
+	}
+	
+	public static double radij(Tocka polozaj){
+		double radij = Math.sqrt(Math.pow(polozaj.vrniElement(1), 2) + Math.pow(polozaj.vrniElement(2), 2) + Math.pow(polozaj.vrniElement(3), 2));
+		return radij;
+		
+	}
 }
-//
-//
-//
-//
