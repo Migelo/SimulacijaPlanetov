d3aea6ee02d4aba926b349c668534b65e25ad394
Migelo <Miha@Mihas-MBP.lan>
2016-06-10 00:04:10 +0200

    Definicija Stanja
    Class stanje se sprehodi po vseh planetih, pokliče funkcijo za izračun
    položaja in jih nariše kjer morajo biti.

--- src/Stanje.java
+++ src/Stanje.java
@@ -1,8 +1,38 @@
+import java.awt.Color;
+import java.awt.Graphics;
 import java.util.ArrayList;
 
+import javax.swing.JPanel;
+
+//tukaj opišemo trenutno stanje, ki ga narišemo na platno
+
+@SuppressWarnings("unused")
 public class Stanje {
-	ArrayList<Planet> planeti = new ArrayList<Planet>();
+	ArrayList<Planet> planeti;
+	boolean sled;
+	Color ozadje;
 	
-
-
+	public Stanje(ArrayList<Planet> planeti){
+		this.planeti = planeti;
+		this.sled = true;
+		this.ozadje = Color.GRAY;
+	}
+	
+	public void narisi(Graphics g){
+		int i = 0;
+		while (i  < 2000){
+			for (Planet planet : planeti){
+				Racunanje.sila2(planet);
+				planet.narisi(g);
+				System.out.println(planet.vrniPolozaj());
+			}
+//			try {
+//				Thread.sleep(40);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			i++;
+		}
+	}
 }
