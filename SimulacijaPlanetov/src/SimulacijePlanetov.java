1a02452d612fb90a6874ab0e1bf8dd8e71dd6323
Migelo <Miha@Mihas-MacBook-Pro.local>
2016-06-14 19:39:37 +0200

    Preimenuj točke v inicializaciji.
    Razumljivo poimenovanje točk.

--- SimulacijaPlanetov/src/SimulacijePlanetov.java
+++ SimulacijaPlanetov/src/SimulacijePlanetov.java
@@ -4,23 +4,21 @@ public class SimulacijePlanetov {
 
 	public static void main(String[] args) {
 		
-		Tocka a = new Tocka(1, 0, 0);
 		Tocka prazna = new Tocka(0,0,0);
-		//Tocka c = new Tocka(0, Math.sqrt(1/Racunanje.radij(a)),0);
-		Tocka c = new Tocka(0, 1.1,0);
-		Planet mars = new Planet(a, c, prazna);
 		
-		Tocka aa = new Tocka(.75, 0, 0);
-		Tocka cc = new Tocka(0, 1.3, 0);
-		Planet zemlja = new Planet(aa, cc, prazna);
+		Tocka marsPolozaj = new Tocka(1, 0, 0);
+		Tocka marsHitrost = new Tocka(0, 1.1,0);
+		Planet mars = new Planet(marsPolozaj, marsHitrost, prazna);
+		
+		Tocka zemljaPolozaj = new Tocka(.75, 0, 0);
+		Tocka zemljaHitrost = new Tocka(0, 1.3, 0);
+		Planet zemlja = new Planet(zemljaPolozaj, zemljaHitrost, prazna);
 
 		ArrayList<Planet> planeti = new ArrayList<Planet>();
 		planeti.add(mars);
 		planeti.add(zemlja);
 		Stanje stanje = new Stanje(planeti);
 		Okno frame = new Okno(stanje);
-		frame.setVisible(true);
-		System.out.println(a);
-		
+		frame.setVisible(true);		
 	}
 }
