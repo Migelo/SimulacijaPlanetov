3fba8470f90c461301c0701a848f3e5022b85dc4
Migelo <Miha@Mihas-MacBook-Pro.local>
2016-06-14 19:09:01 +0200

    Preimenuj planet v razumljivo ime.
    Preimenuj Planet iz b v mars.

--- SimulacijaPlanetov/src/SimulacijePlanetov.java
+++ SimulacijaPlanetov/src/SimulacijePlanetov.java
@@ -8,14 +8,14 @@ public class SimulacijePlanetov {
 		Tocka prazna = new Tocka(0,0,0);
 		//Tocka c = new Tocka(0, Math.sqrt(1/Racunanje.radij(a)),0);
 		Tocka c = new Tocka(0, 1.1,0);
-		Planet b = new Planet(a, c, prazna);
+		Planet mars = new Planet(a, c, prazna);
 		
 		Tocka aa = new Tocka(.75, 0, 0);
 		Tocka cc = new Tocka(0, 1.3, 0);
 		Planet zemlja = new Planet(aa, cc, prazna);
 
 		ArrayList<Planet> planeti = new ArrayList<Planet>();
-		planeti.add(b);
+		planeti.add(mars);
 		planeti.add(zemlja);
 		Stanje stanje = new Stanje(planeti);
 		Okno frame = new Okno(stanje);
